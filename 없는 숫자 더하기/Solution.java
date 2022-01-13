b37cf57ee5f835e15a3330d803d2ac53de8b50c5
subinan <53176366+subinan@users.noreply.github.com>
2022-01-14 03:21:24 +0900

    Update Solution.java
    중복 x

--- 없는 숫자 더하기/Solution.java
+++ 없는 숫자 더하기/Solution.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int[] numbers) {
-        int[] chk = new int[10];
         int ans = 0;
+        int add = 0;
         
         for(int n:numbers)
-            ++chk[n];
-        
-        for (int i = 0; i < 10; i++)
-            ans += (chk[i] == 0 ? i : 0);
-        
+            add += n;
+        ans = 45 - add;
         return ans;
     }
 }
