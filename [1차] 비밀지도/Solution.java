7f4d62b5e4681dad9201ce93f813a72dd4245ad5
subinan <53176366+subinan@users.noreply.github.com>
2022-01-23 00:04:38 +0900

    |

--- [1차] 비밀지도/Solution.java
+++ [1차] 비밀지도/Solution.java
@@ -1,18 +1,16 @@
 class Solution {
-    public String getMap(int n, int m1, int m2) {
+    public String getMap(int n, int m) {
         char[] map = new char[n];
         char[] element = { ' ', '#' };
         int r;
         
         int idx = n;
         while (idx > 0) {
-            if (m1 > 0 || m2 > 0) {
-                r = m1 % 2 + m2 % 2 > 0 ? 1 : 0;
-                map1[--idx] = element[r];
-                m1 /= 2;
-                m2 /= 2;
+            if (m > 0) {
+                map[--idx] = element[m % 2];
+                m /= 2;
             } else {
-                map1[--idx] = element[0];
+                map[--idx] = element[0];
             }
         }
         return String.valueOf(map);
@@ -22,8 +20,38 @@ class Solution {
         String[] answer = new String[n];
         
         for (int i = 0; i < n; i++) {
-            answer[i] = getMap(n, arr1[i], arr2[i]);
+            answer[i] = getMap(n, arr1[i] | arr2[i]); // | 연산 활용
         }
         return answer;
     }
 }
+
+// class Solution {
+//     public String getMap(int n, int m1, int m2) {
+//         char[] map = new char[n];
+//         char[] element = { ' ', '#' };
+//         int r;
+        
+//         int idx = n;
+//         while (idx > 0) {
+//             if (m1 > 0 || m2 > 0) {
+//                 r = m1 % 2 + m2 % 2 > 0 ? 1 : 0;
+//                 map1[--idx] = element[r];
+//                 m1 /= 2;
+//                 m2 /= 2;
+//             } else {
+//                 map1[--idx] = element[0];
+//             }
+//         }
+//         return String.valueOf(map);
+//     }
+    
+//     public String[] solution(int n, int[] arr1, int[] arr2) {
+//         String[] answer = new String[n];
+        
+//         for (int i = 0; i < n; i++) {
+//             answer[i] = getMap(n, arr1[i], arr2[i]);
+//         }
+//         return answer;
+//     }
+// }
