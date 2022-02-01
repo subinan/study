8a7fd1525a61b76404a642fc015984f757660711
subinan <53176366+subinan@users.noreply.github.com>
2022-02-01 16:16:22 +0900

    Create Solution.java

---
+++ 자연수 뒤집어 배열로 만들기/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long n) {
+        List<Integer> list = new ArrayList<>();
+
+        while (n > 9) {
+            list.add((int)(n % 10));
+            n /= 10;
+        }
+        list.add((int)n);
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
