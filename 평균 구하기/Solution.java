ec00f6ef7935064d0909eb87a11cd744f2a6ee36
subinan <53176366+subinan@users.noreply.github.com>
2022-02-06 21:28:41 +0900

    Create Solution.java

---
+++ 평균 구하기/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for (int n : arr) {
+            answer += n;
+        }
+        answer /= arr.length;
+        return answer;
+    }
+}
