6a6dd5db2bf4fd7314b84d6eb30ea0f1a4f85aa1
subinan <53176366+subinan@users.noreply.github.com>
2022-03-20 00:49:57 +0900

    Update Main.java

--- boj/2504/Main.java
+++ boj/2504/Main.java
@@ -1,60 +1,53 @@
-// https://loosie.tistory.com/349
-
-import java.io.*;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String in = scanner.next();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line = br.readLine();
+        int mul = 1;
+        int result = 0;
+        boolean flag = true;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < in.length(); i++) {
+            switch (in.charAt(i)) {
 
-		Stack<Character> st = new Stack<>();
-		boolean flag = true; 
-		int answer = 0;
-		int cnt = 1;
-		for(int i = 0; i < line.length(); i++) {
-			char cur = line.charAt(i);
-			if(cur == '(') {
-				st.push(cur);
-				cnt *= 2;
-			}
-			else if(cur == '[') {
-				st.push(cur);
-				cnt *= 3;
-			}
-			else {
-				if(cur == ')') {
-					if(st.isEmpty() || st.peek() != '(') {
-						flag = false;
-						break;
-					}
-					if(line.charAt(i - 1) =='(') {
-						answer += cnt;
-					}
-					st.pop();
-					cnt /= 2;
-				}else if(cur == ']') {
-					if(st.isEmpty() || st.peek() != '[') {
-						flag = false;
-						break;
-					}
-					if(line.charAt(i-1) == '[') {
-						answer += cnt;
-					}
-					st.pop();
-					cnt /= 3;
-				}
-				else {
-					flag = false;
-					break;
-				}
-			}
-		}
-		if(!flag || !st.isEmpty()) {
-			System.out.println(0);
-		}else {
-			System.out.println(answer);
-		}
-	}
+                case '(':
+                    stack.push('(');
+                    mul *= 2;
+                    break;
+
+                case '[':
+                    stack.push('[');
+                    mul *= 3;
+                    break;
+
+                case ')':
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        result = 0;
+                        flag = false;
+                        break;
+                    }
+
+                    if (in.charAt(i - 1) == '(') result += mul;
+                    stack.pop();
+                    mul /= 2;
+                    break;
+
+                case ']':
+                    if (stack.isEmpty() || stack.peek() != '[') {
+                        result = 0;
+                        flag = false;
+                        break;
+                    }
+
+                    if (in.charAt(i - 1) == '[') result += mul;
+                    stack.pop();
+                    mul /= 3;
+                    break;
+            }
+        }
+
+        System.out.println(!flag || !stack.isEmpty() ? 0 : result);
+    }
 }
